4830f45ef9f796ea58274ac293d321b066229c89
Chris Steele <chris.steele@kineo.com>
2016-06-24 13:36:08 +0100

    Fix for use with adapt-contrib-trickle

--- js/adapt-inlineFeedbackQuestionView.js
+++ js/adapt-inlineFeedbackQuestionView.js
@@ -4,41 +4,39 @@ define([
 ], function(Adapt, QuestionView) {
 
     var InlineFeedbackComponentView = {
-    	postRender: function() {
-    		// shuffle feedback down DOM
-    		this.$('.'+this.model.get('_component')+'-inner').append(this.$('.'+this.model.get('_component')+'-feedback'));
+        postRender: function() {
+            // shuffle feedback down DOM
+            this.$('.' + this.model.get('_component') + '-inner').append(this.$('.' + this.model.get('_component') + '-feedback'));
 
-    		QuestionView.prototype.postRender.call(this);
+            QuestionView.prototype.postRender.call(this);
 
             if (this.model.get('_isSubmitted')) {
-                this.$('.'+this.model.get('_component')+'-feedback').addClass('show-feedback');  
-                this.$('.'+this.model.get('_component')+'-feedback-title').addClass('component-feedback-title'); 
+                this.$('.' + this.model.get('_component') + '-feedback').addClass('show-feedback');
+                this.$('.' + this.model.get('_component') + '-feedback-title').addClass('component-feedback-title');
             }
-    	},
+        },
 
-		showFeedback: function() {
-			QuestionView.prototype.showFeedback.call(this);
+        showFeedback: function() {
+            QuestionView.prototype.showFeedback.call(this);
 
             if (this.model.get('_canShowFeedback')) {
 
-                this.$('.'+this.model.get('_component')+'-feedback').addClass('show-feedback'); 
-    			
-    			this.$('.'+this.model.get('_component')+'-feedback-title')
+                this.$('.' + this.model.get('_component') + '-feedback').addClass('show-feedback');
+
+                this.$('.' + this.model.get('_component') + '-feedback-title')
                     .html(this.model.get('feedbackTitle')).a11y_text()
                     .addClass('component-feedback-title');
 
-    			this.$('.'+this.model.get('_component')+'-feedback-message').html(this.model.get('feedbackMessage')).a11y_text();
+                this.$('.' + this.model.get('_component') + '-feedback-message').html(feedbackMessage).a11y_text();
 
-    			_.delay(_.bind(function() {
-                    var block = this.model.getParent();
+                Adapt.trigger('trickle:resize');
+
+                _.delay(_.bind(function() {
                     var selector = '.' + this.model.get('_id') + ' .' + this.model.get('_component') + '-feedback';
-                    if (block.has('_trickle') && block.get('_trickle')._isEnabled) {
-                        selector = '.' + block.get('_id') + ' .trickle-button-component';
-                    }
                     Adapt.scrollTo(selector, { duration: 500 });
                 }, this), 250);
             }
-		}
+        }
     };
 
     return InlineFeedbackComponentView;
