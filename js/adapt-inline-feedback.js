9184c93b04c7e4803ce6b33f20cd9342605002b3
Guy Willis <guy.willis@kineo.com>
2019-09-12 09:58:05 +0100

    Converted tabs to spaces, indentation amends

--- js/adapt-inline-feedback.js
+++ js/adapt-inline-feedback.js
@@ -1,16 +1,17 @@
 define([
-    'coreJS/adapt',
-    'coreViews/questionView',
-    './adapt-inlineFeedbackQuestionView'
+  'coreJS/adapt',
+  'coreViews/questionView',
+  './adapt-inlineFeedbackQuestionView'
 ], function(Adapt, QuestionView, InlineFeedbackQuestionView) {
 
-    var QuestionViewInitialize = QuestionView.prototype.initialize;
+  var QuestionViewInitialize = QuestionView.prototype.initialize;
 
-    QuestionView.prototype.initialize = function(options) {
-    	if (this.model.get('_canShowFeedback')) {
-        	_.extend(this, InlineFeedbackQuestionView);
-    	}
+  QuestionView.prototype.initialize = function(options) {
+    if (this.model.get('_canShowFeedback')) {
+      _.extend(this, InlineFeedbackQuestionView);
+    }
 
-        return QuestionViewInitialize.apply(this, arguments);
-    };
+    return QuestionViewInitialize.apply(this, arguments);
+  };
+
 });
--- js/adapt-inlineFeedbackQuestionView.js
+++ js/adapt-inlineFeedbackQuestionView.js
@@ -1,120 +1,122 @@
 define([
-    'coreJS/adapt',
-    'coreViews/questionView'
+  'coreJS/adapt',
+  'coreViews/questionView'
 ], function(Adapt, QuestionView) {
 
-    var InlineFeedbackComponentView = {
-        postRender: function() {
-            // position feedback after component-widget if applicable
-            if (this.$('.component-feedback').length > 0) {
-                this.$('.component-inner').append(this.$('.component-feedback'));
-            }
+  var InlineFeedbackComponentView = {
 
-            QuestionView.prototype.postRender.call(this);
+    postRender: function() {
+      // position feedback after component-widget if applicable
+      if (this.$('.component-feedback').length > 0) {
+        this.$('.component-inner').append(this.$('.component-feedback'));
+      }
 
-            if (this.model.get('_isSubmitted')) {
-                this.getFBElement().addClass('show-feedback');
+      QuestionView.prototype.postRender.call(this);
 
-                this.populateFeedback();
-            }
-        },
+      if (this.model.get('_isSubmitted')) {
+        this.getFBElement().addClass('show-feedback');
 
-        getFBSelector:function() {
-            var isParentBlock = this.$('.component-feedback').length == 0;
-            var m = isParentBlock ? this.model.getParent() : this.model;
-            return '.' + m.get('_id') + ' .component-feedback';
-        },
+        this.populateFeedback();
+      }
+    },
 
-        getFBElement:function(selector) {
-            var $fb = $(this.getFBSelector());
+    getFBSelector:function() {
+      var isParentBlock = this.$('.component-feedback').length == 0;
+      var m = isParentBlock ? this.model.getParent() : this.model;
+      return '.' + m.get('_id') + ' .component-feedback';
+    },
 
-            return selector ? $(selector, $fb) : $fb;
-        },
+    getFBElement:function(selector) {
+      var $fb = $(this.getFBSelector());
 
-        populateFeedback:function() {
-            var $feedbackMessage = this.getFBElement('.component-feedback-content');
+      return selector ? $(selector, $fb) : $fb;
+    },
 
-            $feedbackMessage.html(this.model.get('feedbackMessage'));
+    populateFeedback:function() {
+      var $feedbackMessage = this.getFBElement('.component-feedback-content');
 
-            if(!this.model.get('feedbackImage')) {
-                this.getFBElement('.component-feedback-graphic').css('display', 'none');
-                $feedbackMessage.css('width', 'auto');
-                return;
-            }
+      $feedbackMessage.html(this.model.get('feedbackMessage'));
 
-            this.getFBElement('.component-feedback-graphic img').attr({ 'src': this.model.get('feedbackImage') });
+      if (!this.model.get('feedbackImage')) {
+        this.getFBElement('.component-feedback-graphic').css('display', 'none');
+        $feedbackMessage.css('width', 'auto');
+        return;
+      }
 
-            if(this.model.get('feedbackImageAlt')) {
-                this.getFBElement('.component-feedback-graphic img').attr({ 'aria-label': this.model.get('feedbackImageAlt')});
-            }
-        },
+      this.getFBElement('.component-feedback-graphic img').attr({ 'src': this.model.get('feedbackImage') });
 
-        showFeedback: function() {
-            QuestionView.prototype.showFeedback.call(this);
+      if(this.model.get('feedbackImageAlt')) {
+        this.getFBElement('.component-feedback-graphic img').attr({ 'aria-label': this.model.get('feedbackImageAlt')});
+      }
+    },
 
-            if (this.model.get('_canShowFeedback')) {
+    showFeedback: function() {
+      QuestionView.prototype.showFeedback.call(this);
 
-                this.getFBElement().addClass('show-feedback');
+      if (this.model.get('_canShowFeedback')) {
 
-                this.populateFeedback();
+        this.getFBElement().addClass('show-feedback');
 
-                var anchorSelector = '.' + this.model.get('_id') + ' .feedback-anchor';
-                var feedbackSelector = this.getFBSelector();
+        this.populateFeedback();
 
-                // now target a focusable element and focus immediately (a11y_focus defers)...
+        var anchorSelector = '.' + this.model.get('_id') + ' .feedback-anchor';
+        var feedbackSelector = this.getFBSelector();
 
-                // try to focus accessible feedback text if applicable
-                if ($(feedbackSelector).length > 0) {
-                    $(feedbackSelector).a11y_focus();
-                } else if ($(anchorSelector).length > 0) { // else try to focus a feedback anchor if present
-                    $(anchorSelector).a11y_focus();
-                } else {// else place focus in a safe place
-                    $('#a11y-focuser').focus();
-                }
+        // now target a focusable element and focus immediately (a11y_focus defers)...
 
-                _.delay(function() {
-                    this.listenToOnce(Adapt, 'page:scrolledTo', this.onScrolledToFeedback);
-
-                    var selector = this.$('.feedback-anchor').length > 0 ? anchorSelector : feedbackSelector;
-                    Adapt.scrollTo(selector, { duration: 500 });
-
-                }.bind(this), 250);
-            }
-        },
-
-        checkQuestionCompletion: function() {
-
-            var isComplete = false;
-
-            if (this.model.get('_isCorrect') || this.model.get('_attemptsLeft') === 0) {
-                isComplete = true;
-            }
-
-            if (isComplete) {
-                // trickle, if used, must be set to listen for _isComplete
-                this.model.set('_isInteractionComplete', true);
-                this.$('.component-widget').addClass('complete show-user-answer');
-            }
-        },
-
-        onScrolledToFeedback:function() {
-            this.setCompletionStatus();
-
-            // we need to kick PLP to update because we've changed the order of setting _isComplete/_isInteractionComplete
-
-            var parentPage = this.model.findAncestor('contentObjects');
-
-            if (parentPage.findDescendants('components').where({'_isAvailable': true, '_isOptional': false, '_isComplete':false}).length == 0) {
-                // if all page components now complete wait for _isComplete to propagate to page then tell PLP to update
-                parentPage.once('change:_isComplete', function() {
-                    Adapt.trigger('pageLevelProgress:update');
-                });
-            } else {
-                // otherwise update PLP as normal
-                Adapt.trigger('pageLevelProgress:update');
-            }
+        // try to focus accessible feedback text if applicable
+        if ($(feedbackSelector).length > 0) {
+          $(feedbackSelector).a11y_focus();
+        } else if ($(anchorSelector).length > 0) { // else try to focus a feedback anchor if present
+          $(anchorSelector).a11y_focus();
+        } else {// else place focus in a safe place
+          $('#a11y-focuser').focus();
         }
-    };
 
-    return InlineFeedbackComponentView;
+        _.delay(function() {
+          this.listenToOnce(Adapt, 'page:scrolledTo', this.onScrolledToFeedback);
+
+          var selector = this.$('.feedback-anchor').length > 0 ? anchorSelector : feedbackSelector;
+          Adapt.scrollTo(selector, { duration: 500 });
+
+        }.bind(this), 250);
+      }
+    },
+
+    checkQuestionCompletion: function() {
+
+      var isComplete = false;
+
+      if (this.model.get('_isCorrect') || this.model.get('_attemptsLeft') === 0) {
+        isComplete = true;
+      }
+
+      if (isComplete) {
+        // trickle, if used, must be set to listen for _isComplete
+        this.model.set('_isInteractionComplete', true);
+        this.$('.component-widget').addClass('complete show-user-answer');
+      }
+    },
+
+    onScrolledToFeedback:function() {
+      this.setCompletionStatus();
+
+      // we need to kick PLP to update because we've changed the order of setting _isComplete/_isInteractionComplete
+
+      var parentPage = this.model.findAncestor('contentObjects');
+
+      if (parentPage.findDescendants('components').where({'_isAvailable': true, '_isOptional': false, '_isComplete':false}).length == 0) {
+        // if all page components now complete wait for _isComplete to propagate to page then tell PLP to update
+        parentPage.once('change:_isComplete', function() {
+          Adapt.trigger('pageLevelProgress:update');
+        });
+      } else {
+        // otherwise update PLP as normal
+        Adapt.trigger('pageLevelProgress:update');
+      }
+    }
+  };
+
+  return InlineFeedbackComponentView;
+
 });
